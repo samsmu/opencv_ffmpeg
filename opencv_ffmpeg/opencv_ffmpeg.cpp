3c6dea527a4b26b1c94e62a28986159cbf8d912c
Sergey Denisov <s.s.denisov@yandex.ru>
2020-06-25 16:55:30 +0400

    fix for new x264 aand FFmpeg and OpenCV 4.1.0
    NAVI-1278

--- opencv_ffmpeg/opencv_ffmpeg.cpp
+++ opencv_ffmpeg/opencv_ffmpeg.cpp
@@ -2,5 +2,8 @@
 #pragma comment(lib, "Wldap32.lib")
 #pragma comment(lib, "Bcrypt.lib")
 #pragma comment(lib, "Secur32.lib")
+#pragma comment(lib, "Mfplat.lib")
+#pragma comment(lib, "Mfuuid.lib")
+#pragma comment(lib, "Strmiids.lib")
 
 #include "cap_ffmpeg_impl.hpp"
